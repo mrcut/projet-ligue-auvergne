60b290ea8e609a621a21726c7f8a7e439893b2af
Yikkme <nicky.thaing.etu@univ-lille.fr>
2023-10-26 11:28:14 +0200

    structure de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import MainRoutes from './routes/MainRoutes';
-
+import { useState } from "react";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import MainRoutes from "./routes/MainRoutes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <BrowserRouter>
-          <Header/>
-          <MainRoutes/>
-         <Footer/>
-        </BrowserRouter>
+        <Header />
+        <MainRoutes />
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
