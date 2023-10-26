9938ebbe5a18a39ef0944833eac145d564a090b1
Yikkme <nicky.thaing.etu@univ-lille.fr>
2023-10-26 13:05:55 +0200

    premier commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import MainRoutes from "./routes/MainRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Home from "../pages/Home";
-
+import ProductList from "../components/products/ProducList";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home></Home>} />
+      <Route path="/productsList" element={<ProductList></ProductList>} />
+
     </Routes>
   );
 };
